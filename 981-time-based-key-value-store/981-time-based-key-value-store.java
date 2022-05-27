b2f1ee3c0943daf409efcb975792b2ed004e4632
sidhaldar98 <sidhaldar98@gmail.com>
2022-05-26 21:10:24 -0700

    Time: 466 ms (5.03%), Space: 223.6 MB (9.73%) - LeetHub

---
+++ 981-time-based-key-value-store/981-time-based-key-value-store.java
@@ -0,0 +1,72 @@
+class TimeMap {
+    Map<String,List<Integer>> mapT;
+    Map<String,List<String>> mapV;
+    public TimeMap() {
+       this.mapT = new HashMap<>(); 
+       this.mapV = new HashMap<>(); 
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if(!this.mapT.containsKey(key)){
+            List<Integer> lT = new ArrayList<>();
+            //lT.add(-1);
+            lT.add(timestamp);
+            List<String> lV = new ArrayList<>();
+            //lV.add("");
+            lV.add(value);
+            this.mapT.put(key,lT);
+            this.mapV.put(key,lV);
+        }
+        else{
+            List<Integer> lT = this.mapT.get(key);
+            List<String> lV = this.mapV.get(key);
+            lT.add(timestamp);
+            lV.add(value);
+            this.mapT.put(key,lT);
+            this.mapV.put(key,lV);
+        }
+    }
+    
+    public String get(String key, int timestamp) {
+       List<Integer> lT = this.mapT.get(key); 
+        List<String> lV = this.mapV.get(key);
+        //System.out.println(lT);
+        //System.out.println(lV);
+       if(lT == null || timestamp < lT.get(0))
+           return "";
+       int L = lT.size();
+       int l = 0, r = L-1;
+        int mid = 0;
+       while(l <= r){
+           mid = (l+r)/2;
+           int val = lT.get(mid);
+           if(val == timestamp)
+               break;
+           else if(val < timestamp){
+               if(mid == (L-1))
+                   break;
+               else if(val < lT.get(mid+1))
+                   if(timestamp < lT.get(mid + 1))
+                        break;
+                   else if(timestamp == lT.get(mid + 1)){
+                       mid = mid + 1;
+                       break;
+                   }
+                   else if(timestamp > lT.get(mid + 1))
+                       l = mid + 1;
+           }
+           else{
+               r = mid - 1;
+           }
+       }
+        System.out.println(mid);
+       return this.mapV.get(key).get(mid);
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
