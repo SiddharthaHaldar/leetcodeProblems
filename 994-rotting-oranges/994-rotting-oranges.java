54fa0cbf38c9f35efe3197489a9df21f2ee54157
sidhaldar98 <sidhaldar98@gmail.com>
2022-06-07 05:19:48 +0000

    Time: 11 ms (7.63%), Space: 43.3 MB (25.74%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -0,0 +1,57 @@
+class Solution {
+    int minutes = 0,freshtorot = 0;
+    public int orangesRotting(int[][] grid) {
+        int r = grid.length, 
+            c = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        System.out.println(q);
+        int fresh = 0,rot = 0, total = 0;
+        for(int x = 0;x < r;x++){
+            for(int y = 0;y < c; y++){
+                if(grid[x][y] != 0){
+                    total += 1;
+                    if(grid[x][y] == 1)
+                        fresh += 1;
+                    else if(grid[x][y] == 2){
+                       rot += 1;
+                       q.add(new int[]{x,y});
+                    }
+                }
+            }
+        }
+        while(!q.isEmpty() && fresh > 0){
+            int size = q.size();
+            for(int i = 0;i < size;i++){
+                int arr[] = q.remove();
+                int x = arr[0],y = arr[1];
+                if(x-1 >= 0 && grid[x-1][y] != 0 && grid[x-1][y] != 2){
+                    q.add(new int[]{x-1,y});
+                    grid[x-1][y] = 2;
+                    fresh -= 1;
+                }
+                if(x+1 < grid.length && grid[x+1][y] != 0 && grid[x+1][y] != 2){
+                    q.add(new int[]{x+1,y});
+                    grid[x+1][y] = 2;
+                    fresh -= 1;
+                }
+                if(y-1 >= 0 && grid[x][y-1] != 0 && grid[x][y-1] != 2){
+                    q.add(new int[]{x,y-1});
+                    grid[x][y-1] = 2;
+                    fresh -= 1;
+                }
+                if(y+1 < grid[0].length && grid[x][y+1] != 0 && grid[x][y+1] != 2){
+                    q.add(new int[]{x,y+1});
+                    grid[x][y+1] = 2;
+                    fresh -= 1;
+                }
+            }
+            minutes += 1;
+        }
+        System.out.println(minutes);
+        System.out.println(freshtorot);
+        //System.out.println(minutes);
+        if(fresh == 0)
+            return minutes;
+        return -1;   
+    }
+}
