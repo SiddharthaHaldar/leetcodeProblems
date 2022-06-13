2e8ffd86cce0979e5ae468855f5622fe1cb6655b
sidhaldar98 <sidhaldar98@gmail.com>
2022-06-13 04:07:17 +0000

    Time: 27 ms (11.93%), Space: 54.2 MB (5.80%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -0,0 +1,58 @@
+class Solution {
+    HashSet<Integer> inserted;
+    int pos = -1;
+    int[] output;
+    HashSet<Integer> visited = new HashSet<Integer>();
+    public int[] findOrder(int numCourses, int[][] pre) {
+        Map<Integer,List<Integer>> adj = new HashMap<>();
+        inserted = new HashSet<Integer>(numCourses);
+        output = new int[numCourses];
+        int r = pre.length;
+        boolean res = true;
+        for(int x = 0; x < r; x++){
+            int course = pre[x][0]; 
+            if(!adj.containsKey(course)){
+                List<Integer> l = new ArrayList<>();
+                l.add(pre[x][1]);
+                adj.put(course,l);
+            }
+            else{
+                adj.get(course).add(pre[x][1]);
+            }
+            if(!adj.containsKey(course))
+                adj.put(course,new ArrayList<>());
+        }
+        System.out.println(adj);
+        for(int x = 0;x < numCourses;x++){
+            if(!inserted.contains(x)){
+                res = res & dfs(x,adj);
+                if(!res)
+                    return new int[0];
+            }
+        }
+        
+        List<Integer> output2 = new ArrayList<Integer>();
+        output2.toArray(new Integer[10]);
+        return output;
+    }
+    public boolean dfs(Integer key,Map<Integer,List<Integer>> map){
+        if(visited.contains(key))
+            return false;
+        visited.add(key);
+        boolean res = true;
+        List<Integer> l = map.getOrDefault(key,new ArrayList<>());
+        for(Integer el : l){
+           if(!inserted.contains(el)){
+               res = res & dfs(el,map);
+               if(!res)
+                   break;
+           }
+        }
+                //Set the value of the key to a an empty list
+                //This is because afetr each traversal if no cycles are detected we know that that course/node can be traveresed sefely without any cycles.
+        visited.remove(key);
+        inserted.add(key);
+        output[++pos] = key;
+        return res;
+    } 
+}
