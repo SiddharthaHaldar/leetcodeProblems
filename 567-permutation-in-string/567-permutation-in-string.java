4d000dcab6abaf85e571b9eefe6ca522da089978
sidhaldar98 <sidhaldar98@gmail.com>
2022-06-21 07:32:19 +0000

    Time: 507 ms (10.76%), Space: 117.7 MB (5.03%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int k = s1.length(),l = s2.length();
+        
+        Map<Character,Integer> maps1 = new HashMap<>();
+        Map<Character,Integer> maps2 = new HashMap<>();
+        
+        for(char c : s1.toCharArray()){
+            if(!maps1.containsKey(c))
+                maps1.put(c,0);
+            maps1.put(c,maps1.get(c)+1);
+        }
+        
+        System.out.println(maps1);
+        int left = 0, right = 0;
+        for(int x =0 ; x < l ; x++){
+           /*if(right - left + 1 == k) {
+              return true; 
+           }
+            else{*/
+                char c = s2.charAt(right);
+                if(!maps1.containsKey(c)){
+                    maps2.clear();
+                    right++;
+                    left = right;
+                }
+                else{
+                    right++;
+                    maps2.put(c,maps2.getOrDefault(c,0)+1);
+                    System.out.println(maps2);
+                    while(maps2.get(c) > maps1.get(c)){
+                        System.out.println("exceeds");
+                        char c2 = s2.charAt(left);
+                        maps2.put(c2,maps2.get(c2)-1);
+                        left++;
+                        //maps2.clear();
+                        //left = right;
+                    }
+                    if(right - left == k)
+                        return true;
+                }
+            //System.out.println(maps2);
+            //}
+        }
+        return false;
+    }
+}
