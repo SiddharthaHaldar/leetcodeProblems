cd30f901a5d1cdde503760a9645c144cc61c30d3
sidhaldar98 <sidhaldar98@gmail.com>
2022-07-24 18:37:01 -0700

    Time: 190 ms (5.03%), Space: 14.8 MB (36.51%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = -1 
+        r = len(s)
+        while(l<r):
+            l += 1
+            r -= 1
+            while((not (ord(s[l].lower()) >= 48 and
+                        ord(s[l].lower()) <= 57) and
+                   not (ord(s[l].lower()) >= 97 and
+                        ord(s[l].lower()) <= 122)) and
+                  l <= len(s) - 1 and l < r):
+                l += 1
+            while((not (ord(s[r].lower()) >= 48 and
+                        ord(s[r].lower()) <= 57) and
+                   not (ord(s[r].lower()) >= 97 and
+                        ord(s[r].lower()) <= 122)) and
+                  r <= len(s) - 1 and l < r):
+                r -= 1
+            
+            '''if(r < l):
+                return True'''
+            print(s[l],s[r])
+            if(s[l].lower() != s[r].lower()):
+                return False
+                
+        return True
