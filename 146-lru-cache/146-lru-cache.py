1caf9b6d082cbb28526c0b7c44f8fd24c007918b
sidhaldar98 <sidhaldar98@gmail.com>
2022-08-04 01:58:46 -0700

    Time: 1281 ms (54.82%), Space: 75.8 MB (50.17%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -14,13 +14,10 @@ class LRUCache:
         self.map = {}
 
     def get(self, key: int) -> int:
-        #print('get',key)
         if(key in self.map):
             node = self.map[key]
             if(self.count > 1):
-                #print(self.count)
                 if(node.next == None):
-                    #print('none')
                     node.prev.next = None
                     self.last = node.prev
                     node.prev = None
@@ -28,7 +25,6 @@ class LRUCache:
                     node.next.prev = node
                     self.head = node
                 elif(node.next != None and node.prev != None):
-                    #print('none,none')
                     node.prev.next = node.next
                     node.next.prev = node.prev
                     node.prev = None
@@ -36,16 +32,9 @@ class LRUCache:
                     node.next.prev = node
                     self.head = node
                     
-            #print(self.head.val,self.last.val)
-            #print(self.map.keys(),self.head.key,self.last.key)
             trav =self.head
-            '''while(trav):
-                print(trav.key,end = ' ')
-                trav = trav.next
-            print("\n")'''
             return node.val
         else:
-            #print("\n")
             return -1
 
     def put(self, key: int, value: int) -> None:
@@ -53,7 +42,6 @@ class LRUCache:
             node = self.map[key]
             if(self.count > 1):
                 if(node.next == None):
-                    #print('none')
                     node.prev.next = None
                     self.last = node.prev
                     node.prev = None
@@ -61,7 +49,6 @@ class LRUCache:
                     node.next.prev = node
                     self.head = node
                 elif(node.next != None and node.prev != None):
-                    #print('none,none')
                     node.prev.next = node.next
                     node.next.prev = node.prev
                     node.prev = None
@@ -70,40 +57,27 @@ class LRUCache:
                     self.head = node
                     
             node.val = value
-            #print(self.head.val,self.last.val)
-            #print('put',key,value)
             self.map[key] = node
-            #print(self.map.keys(),self.head.key,self.last.key)
             trav =self.head
-            '''while(trav):
-                print(trav.key,end = ' ')
-                trav = trav.next
-            print("\n")'''
         else:
             node = Node(key,value)
             self.count += 1
             if(self.count <= self.capacity):
                 if(self.head):
-                    #print("NExceed,HE")
                     node.next = self.head
                     node.next.prev = node
                     self.head = node
                 else:
-                    #print("NExceed,HNone")
                     self.head = node
                     self.last = node
             else:
                 self.count -= 1
-                #print("count",self.count)
                 toBeRemoved = self.last
                 if(self.last.prev == None):
                     self.head = node
                     self.last = node
                 else:
-                    '''print("Exceeded and head not None")
-                    print(self.head.val,self.last.val)'''
                     prevToLast = self.last.prev
-                    #print(prevToLast.val)
                     prevToLast.next = None
                     self.last.prev = None
                     self.last = prevToLast
@@ -111,17 +85,7 @@ class LRUCache:
                     node.next.prev = node
                     self.head = node
                 del self.map[toBeRemoved.key]
-            #print(self.head.val,self.last.val)
-            #print(self.head.prev,self.last.next)
-            #print('put',key,value)
             self.map[key] = node
-            #print(self.map.keys(),self.head.key,self.last.key)
-            trav = self.head
-            s = ""
-            '''while(trav):
-                print(trav.key,end = ' ')
-                trav = trav.next
-            print("\n")'''
 
 
 
